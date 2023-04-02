4d27d9e1d86f485236afc0ec55ee59c2ce99f084
AMAN MAHESHWARI <98646204+Amanmahe@users.noreply.github.com>
2023-04-02 17:03:56 +0530

    Update settings.py

--- mac/settings.py
+++ mac/settings.py
@@ -72,12 +72,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'mac.wsgi.application'
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
